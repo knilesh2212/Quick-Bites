47e7a96f53aeb6532a4feeedb0ceb0093fb8758b
knilesh2212 <knilesh2212@gmail.com>
2025-01-17 21:36:01 +0530

    edit quickBite

--- quickBites/lib/main.dart
+++ quickBites/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/services/auth/auth_gate.dart';
@@ -10,20 +11,21 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(
-    MultiProvider(
-      providers: [
-        // theme provider
-        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+  runApp(DevicePreview(
+    builder: (context) {
+      return MultiProvider(
+        providers: [
+          // theme provider
+          ChangeNotifierProvider(create: (context) => ThemeProvider()),
 
-        // restaurant
-        ChangeNotifierProvider(create: (context) => Restaurant()),
-      ],
-      child: const MyApp(),
-    ),
-  );
+          // restaurant
+          ChangeNotifierProvider(create: (context) => Restaurant()),
+        ],
+        child: const MyApp(),
+      );
+    },
+  ));
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
